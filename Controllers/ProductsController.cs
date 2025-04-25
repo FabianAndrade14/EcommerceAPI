445b1064d83d8218282e092dd2801b75c42c1037
FabianAndrade14 <fabianandresm10@gmail.com>
2025-04-25 16:43:37 -0500

    feat(master): Added the endpoints of get and post to the ProductsController.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcommerceAPI.Models;
+using EcommerceAPI.Data;
 
 namespace EcommerceAPI.Controllers
 {
-    public class ProductsController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly EcommerceContext _context;
+
+        public ProductsController(EcommerceContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+
         }
     }
 }
