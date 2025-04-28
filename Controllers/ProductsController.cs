e748935da965405a7eacd1148fb2bd5796de8f4f
FabianAndrade14 <fabianandresm10@gmail.com>
2025-04-28 14:43:25 -0500

    fix(master): Added the REST to the productsController.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -38,5 +38,25 @@ namespace EcommerceAPI.Controllers
             return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
 
         }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id) return BadRequest();
+            
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
