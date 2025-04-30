4a4e9eeae407b67cf836f181868cae8f0d98fb1b
FabianAndrade14 <fabianandresm10@gmail.com>
2025-04-30 00:25:10 -0500

    fix(master): UserService ready to being use.

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,35 @@
-﻿namespace EcommerceAPI.Services
+﻿using EcommerceAPI.Data;
+using EcommerceAPI.DTOs;
+using EcommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAPI.Services
 {
     public class UserService
     {
+        private readonly EcommerceContext _context;
+        public UserService(EcommerceContext context) { _context = context; }
+        public async Task<User?> RegisterUserAsync(UserRegisterDTO dto)
+        {
+            if (_context.Users.Any(u => u.Email == dto.Email)) return null;
+            var user = new User
+            {
+                UserName = dto.Username,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<User?> AuthenticateAsync(UserLoginDTO dto)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+            if(user is null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash)) return null;
+            return user;
+        }
     }
 }
